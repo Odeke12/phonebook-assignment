1a1e7ae51f599d6835863eb497be70e3158adb21
Odeke12 <trevodex@gmail.com>
2022-02-26 11:30:40 +0300

    Second commit

--- server.js
+++ server.js
@@ -2,24 +2,62 @@ const express = require('express')
 const app = express()
 require("dotenv").config();
 mongoose = require('mongoose')
-const port = process.env.PORT || 3000
-const studentRoutes = require('./routes/students')
+// const studentRoutes = require('./routes/students')
 const {MongoClient} = require('mongodb');
+const { interfaces } = require('mocha');
 
 const client = new MongoClient(process.env.DATABASE);
 
-mongoose.connect(
-    process.env.DATABASE,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        // useCreateIndex: true,
-});
 
-mongoose.connection
-    .on('open', () => {
-        console.log('Mongoose connection open');
+const contacts = []
+
+// mongoose.connect(
+//     process.env.DATABASE,
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         // useCreateIndex: true,
+// });
+
+// mongoose.connection
+//     .on('open', () => {
+//         console.log('Mongoose connection open');
+//     })
+//     .on('error', (err) => {
+//         console.log(`Connection error: ${err.message}`);
+//     });  
+
+ app.use(express.json());                                     
+ app.use(express.urlencoded({extended: true}));               
+ app.use(express.text());                                    
+ app.use(express.json({ type: 'application/json'}));  
+
+ const sendMessage = (res,status_num,message) => {
+    res.status(status_num).send(message);
+}
+
+    app.get('/',(req,res) => {
+        if(contacts.length == 0){
+            console.log('No numbers yet')
+            sendMessage(res,400,"Empty phone book")
+        }else{
+            sendMessage(res,400,contacts) 
+        }
     })
-    .on('error', (err) => {
-        console.log(`Connection error: ${err.message}`);
+
+    app.post('/',(req,res) => {
+        if(req.body.first_name == undefined || req.body.last_name == undefined || req.body.phone_number == undefined){
+            sendMessage(res,500,"Please enter all fields")
+        }else{
+            contacts.push(req.body)
+            sendMessage(res,200,contacts)
+        }
+    })
+
+    const port = process.env.port || 3000
+
+    app.listen(port, () =>{
+        console.log(`Server running on port ${port}`);
     });
+    
+    module.exports = app
